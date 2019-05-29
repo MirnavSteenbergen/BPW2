755ed9ab805697a3dae52b7b65c285cd044521ab
MirnavSteenbergen <mirna.van.s@gmail.com>
2019-05-29 14:15:58 +0200

    added char controller en scene

---
+++ BPW2/Assets/scripts/PlayerController1.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController1 : MonoBehaviour
+{
+    public KeyCode upKey, downKey, leftKey, rightKey, interactionKey;
+    public float moveSpeed = 6f;
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(upKey))
+        {
+            transform.position += Vector3.up * Time.deltaTime * moveSpeed;
+        }
+        else if (Input.GetKey(leftKey))
+        {
+            transform.position += Vector3.left * Time.deltaTime * moveSpeed;
+        }
+        else if (Input.GetKey(downKey))
+        {
+            transform.position += Vector3.down * Time.deltaTime * moveSpeed;
+        }
+        else if (Input.GetKey(rightKey))
+        {
+            transform.position += Vector3.right * Time.deltaTime * moveSpeed;
+        }
+
+        if (Input.GetKeyDown(interactionKey))
+        {
+            //interact with object
+        }
+    }
+}
