fdcc52d9e41d687e032c6768c00fc408f5ffdef3
MirnavSteenbergen <mirna.van.s@gmail.com>
2019-06-11 23:59:42 +0200

    added assets icons and player character also in scene

---
+++ BPW2/Assets/scripts/timer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour
+{
+    public Text uiTime;
+    public float StartTime;
+    public float CurrentTime;
+    public float speed = 1;
+
+    private void Start()
+    {
+        CurrentTime = StartTime;
+        uiTime = GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+        CurrentTime -= Time.deltaTime * speed;
+        string minutes = Mathf.Floor((CurrentTime % 3600)/60).ToString("00");
+        string seconds = Mathf.Floor(CurrentTime % 60).ToString("00");
+        uiTime.text = minutes + ":" + seconds;
+    }
+}
